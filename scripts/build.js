1f9b2baf8a8093b7e7b277dfbaaea85325cc9405
Simon Chan <cnsimonchan@live.com>
2021-01-02 22:50:00 +0800

    chore: update template and dependencies

--- scripts/build.js
+++ scripts/build.js
@@ -6,7 +6,24 @@ if (process.platform === 'win32') {
   command += '.cmd';
 }
 
-console.log("Compiling for CommonJS");
+console.log("Compiling to esm and dts");
+
+if (fs.existsSync('esm')) {
+  fs.rmdirSync('esm', { recursive: true });
+}
+
+if (fs.existsSync('dts')) {
+  fs.rmdirSync('dts', { recursive: true });
+}
+
+cp.spawnSync(command, [
+  '--project', 'tsconfig.src.json',
+  '--listEmittedFiles', 'true',
+], {
+  stdio: 'inherit'
+});
+
+console.log("Compiling to cjs");
 
 if (fs.existsSync('cjs')) {
   fs.rmdirSync('cjs', { recursive: true });
@@ -14,12 +31,11 @@ if (fs.existsSync('cjs')) {
 
 const result = cp.spawnSync(command, [
   '--project', 'tsconfig.src.json',
-  '--importHelpers', 'true',
-  '--removeComments', 'true',
-  '--target', 'ES5',
   '--module', 'CommonJS',
-  '--sourceMap', 'true',
   '--outDir', 'cjs',
+  '--declaration', 'false',
+  '--declarationDir', 'null',
+  '--declarationMap', 'false',
   '--listEmittedFiles', 'true',
 ], {
   stdio: 'inherit'
@@ -29,40 +45,3 @@ if (result.status !== 0) {
   console.error('Compiling failed');
   console.error(result.error);
 }
-
-console.log("Compiling for ESModule");
-
-if (fs.existsSync('esm')) {
-  fs.rmdirSync('esm', { recursive: true });
-}
-
-cp.spawnSync(command, [
-  '--project', 'tsconfig.src.json',
-  '--importHelpers', 'true',
-  '--removeComments', 'true',
-  '--target', 'ES5',
-  '--module', 'ESNext',
-  '--moduleResolution', 'Node',
-  '--sourceMap', 'true',
-  '--outDir', 'esm',
-  '--listEmittedFiles', 'true',
-], {
-  stdio: 'inherit'
-});
-
-console.log("Generating declarations");
-
-if (fs.existsSync('dts')) {
-  fs.rmdirSync('dts', { recursive: true });
-}
-
-cp.spawnSync(command, [
-  '--project', 'tsconfig.src.json',
-  '--outDir', 'dts',
-  '--declaration', 'true',
-  '--declarationMap', 'true',
-  '--emitDeclarationOnly', 'true',
-  '--listEmittedFiles', 'true',
-], {
-  stdio: 'inherit'
-});
